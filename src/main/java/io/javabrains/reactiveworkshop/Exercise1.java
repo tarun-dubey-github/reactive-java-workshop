4bf6f3f1414f24023fb58adda00e04b0a89cdceb
SG0314458 <tarun.dubey.ctr@sabre.com>
2022-07-05 09:20:22 +0530

    completed exercise 1

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -1,30 +1,53 @@
 package io.javabrains.reactiveworkshop;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Exercise1 {
 
     public static void main(String[] args) {
 
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
-        // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        System.out.println("-----------  Print all numbers in the intNumbersStream stream");
 
-        // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .forEach(i -> System.out.println(i));
 
-        // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
 
-        //  Print the first number in intNumbersStream that's greater than 5.
-        //  If nothing is found, print -1
-        // TODO: Write code here
+        System.out.println("-----------  Print numbers from intNumbersStream that are less than 5");
+        StreamSources.intNumbersStream()
+                .filter(i -> i < 5)
+                .forEach(i -> System.out.println(i));
 
-        // Print first names of all users in userStream
-        // TODO: Write code here
 
-        // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        System.out.println("-----------  Print the second and third numbers in intNumbersStream that's greater than 5");
+        final AtomicInteger idx = new AtomicInteger(0);
+        record Rec(Integer idx, Integer val) {
+        }
+        StreamSources.intNumbersStream()
+                .filter(i -> i > 5)
+                .map(i -> new Rec(idx.getAndIncrement(), i))
+                .filter(rec -> rec.idx == 1 || rec.idx == 2)
+                .forEach(i -> System.out.println(i.val));
 
+
+        System.out.println("-----------   Print the first number in intNumbersStream that's greater than 5.If nothing is found, print -1");
+        System.out.println(StreamSources.intNumbersStream()
+                .filter(i -> i > 55)
+                .findFirst().orElse(-1));
+
+
+        System.out.println("-----------  Print first names of all users in userStream");
+        StreamSources.userStream()
+                .forEach(u -> System.out.println(u.getFirstName()));
+
+        System.out.println("----------- Print first names in userStream for users that have IDs from number stream");
+
+        List<Integer> ids = StreamSources.intNumbersStream().toList();
+        StreamSources.userStream()
+                .filter(u -> ids.contains(u.getId()))
+                .forEach(u -> System.out.println(u.getId() + "----" + u.getFirstName()));
     }
 
 }
